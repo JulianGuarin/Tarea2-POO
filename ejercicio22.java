6d588d278b9587e8ec1ebfb2cd82e361967bd7bc
JulianGuarin <112726083+JulianGuarin@users.noreply.github.com>
2022-09-03 15:53:00 -0500

    Add files via upload

---
+++ ejercicio22.java
@@ -0,0 +1,28 @@
+package tarea2;
+import java.util.Scanner;
+public class ejercicio22 {
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		String nombre_empleado;
+		int salario_hora;
+		int numero_horas;
+		System.out.println("Escriba el nombre del empleado: ");
+		nombre_empleado = entrada.next();
+		System.out.println("Escriba el salario por hora del empleado: ");
+		salario_hora = entrada.nextInt();
+		System.out.println("Escriba el numero de horas trabajadas ");
+		numero_horas = entrada.nextInt();
+		if ((salario_hora * numero_horas )> 450000) {
+			System.out.println(nombre_empleado);
+			System.out.println(salario_hora * numero_horas);
+		}
+		else {
+			System.out.println(nombre_empleado);
+			
+		}
+		
+		
+	}
+
+}
+//melo
